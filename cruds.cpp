8806a7d433785189efd1715d60b37ee88f0a628c
JonatanAn12 <jonatan25sanchez@gmail.com>
2024-02-01 20:41:13 -0500

    este es mi segundo comit

--- cruds.cpp
+++ cruds.cpp
@@ -28,22 +28,22 @@ void imprimirPersona(const Persona& persona) {
 int main() {
     vector<Persona> personas;
 
-    personas.push_back(Persona("Juan", 25));
-    personas.push_back(Persona("Maria", 30));
+    personas.push_back(Persona("fernando", 25));
+    personas.push_back(Persona("Matildha", 30));
 
     cout << "Personas en la lista:" << endl;
     for (const auto& persona : personas) {
         imprimirPersona(persona);
     }
 
-    string nombreActualizar = "Juan";
+    string nombreActualizar = "fernando";
     auto it = find_if(personas.begin(), personas.end(), [nombreActualizar](const Persona& p) {
         return p.getNombre() == nombreActualizar;
     });
 
     if (it != personas.end()) {
         it-> edad = 26;
-        cout << "Edad de Juan actualizada." << endl;
+        cout << "Edad de fernando actualizada." << endl;
     } else {
         cout << "Persona no encontrada." << endl;
     }
@@ -53,13 +53,13 @@ int main() {
         imprimirPersona(persona);
     }
 
-    string nombreEliminar = "Maria";
+    string nombreEliminar = "Matildha";
     auto eraseIt = remove_if(personas.begin(), personas.end(), [nombreEliminar](const Persona& p) {
         return p.getNombre() == nombreEliminar;
     });
 
     personas.erase(eraseIt, personas.end());
-    cout << "Persona Maria eliminada." << endl;
+    cout << "Persona Matildha eliminada." << endl;
 
     cout << "Personas después de la eliminación:" << endl;
     for (const auto& persona : personas) {
