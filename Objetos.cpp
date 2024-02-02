5a473cdf1a02c47d96c722883ad3cf10dce02eda
JonatanAn12 <jonatan25sanchez@gmail.com>
2024-02-02 06:21:27 -0500

    segundo commit en Objetos cpp

--- Objetos.cpp
+++ Objetos.cpp
@@ -13,15 +13,15 @@ class alumnos {
 int main() {
     
  alumnos alumno1;
- alumno1.nombre = "Jaime";
- alumno1.edad = 23;
- alumno1.nota = 7.2;
+ alumno1.nombre = "Pedro";
+ alumno1.edad = 24;
+ alumno1.nota = 7.8;
  return 0;
 
  alumnos alumno2;
  alumno2.nombre = "Luis";
  alumno2.edad = 34;
- alumno2.nota= 8.2;
+ alumno2.nota= 6.2;
  return 0;
 
  cout << alumno1.nombre << endl;
