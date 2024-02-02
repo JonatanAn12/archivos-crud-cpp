abab92439e9739fe3973513771d19a3bd708f8a6
JonatanAn12 <jonatan25sanchez@gmail.com>
2024-02-02 06:15:32 -0500

    octavo commit en Encapsulamiento cpp

--- PooEncap.cpp
+++ PooEncap.cpp
@@ -32,8 +32,8 @@ class persona {
 int main()
 {
 persona persona1;
-persona1.setNombre("Paola");
-persona1.setEdad(28);
+persona1.setNombre("Nikoll");
+persona1.setEdad(29);
 
 cout << "Nombre: " << persona1.getNombre() << endl;
 cout << "Edad: " << persona1.getEdad() << endl;
