6d6855c4b156b4680e05d17a35cd4f32b87532d0
JonatanAn12 <jonatan25sanchez@gmail.com>
2024-02-02 05:24:07 -0500

    segundo commit en clases cpp

--- poo.cpp
+++ poo.cpp
@@ -14,8 +14,8 @@ class persona {
 int main ()
 {
     persona persona1;
-    persona1.nombre = "Nikoll";
-    persona1.edad = 22;
+    persona1.nombre = "laura";
+    persona1.edad = 23;
     persona1.saludar();
    
     return 0;
