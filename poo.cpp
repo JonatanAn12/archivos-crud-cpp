16fa8d2f6aec50aa5ece5ae59869e081165ecb1d
JonatanAn12 <jonatan25sanchez@gmail.com>
2024-02-02 05:38:23 -0500

    decimo commit en clases cpp

--- poo.cpp
+++ poo.cpp
@@ -14,8 +14,8 @@ class persona {
 int main ()
 {
     persona persona1;
-    persona1.nombre = "Paola";
-    persona1.edad = 29;
+    persona1.nombre = "Sanchez";
+    persona1.edad = 20;
     persona1.saludar();
    
     return 0;
